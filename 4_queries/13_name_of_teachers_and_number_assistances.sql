f64fd9d4b699819d167bd54bf9bc7673bc3ab5d1
saadkureshi <saq619@gmail.com>
2021-01-05 22:04:47 +0000

    New queries added for teachers and requests

---
+++ 4_queries/13_name_of_teachers_and_number_assistances.sql
@@ -0,0 +1,8 @@
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+JOIN students ON assistance_requests.student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teacher, cohort
+ORDER BY teacher;
