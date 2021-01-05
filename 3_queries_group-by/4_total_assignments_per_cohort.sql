1eaa8617c4b7a07e0543facdd54210e26c721945
saadkureshi <saq619@gmail.com>
2021-01-05 01:00:00 +0000

    New queries added

---
+++ 3_queries_group-by/4_total_assignments_per_cohort.sql
@@ -0,0 +1,5 @@
+SELECT cohorts.name as cohort_name, count(assignment_submissions.*) as total_submissions
+FROM cohorts JOIN students ON cohorts.id = students.cohort_id
+JOIN assignment_submissions ON students.id = student_id
+GROUP BY cohort_name
+ORDER BY total_submissions DESC;
